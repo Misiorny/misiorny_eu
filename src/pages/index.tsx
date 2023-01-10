cb14856faa9cbef6fd944b6aa69d560f14cf1c12
Misiorny <krzystan@wp.pl>
2023-01-10 18:33:55 +0100

    Create color palette

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-
-
 const Home = () => {
   return (
-    <div className='flex  bg-amber-300 shadow-xl drop-shadow-2xl shadow-blue-700 mx-auto max-w-md mt-3'>
-      <p className='mx-auto px-4 py-2 mt-3 text-3xl '>Tu bedzie kiedyś nowa strona </p>
+    <div className="flex bg-amber-300 shadow-xl drop-shadow-2xl shadow-blue-700 mx-auto max-w-md mt-3 ">
+      <p className="mx-auto px-4 py-2 mt-3 text-3xl ">
+        Tu bedzie kiedyś nowa strona{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
